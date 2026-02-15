292be51583f8d510e7e8244f0398daa9767f596d
Rachit-Thakuriya07 <rachit77215@gmail.com>
2026-02-15 11:52:05 +0530

    first commit

---
+++ CodInClub/level1_q2.java
@@ -0,0 +1,15 @@
+package CodInClub;
+
+public class level1_q2 {
+   
+    public static void main(String[] args) {
+        int maths = 94;
+        int physics = 95;
+        int chemistry = 96;
+
+        double average = (maths + physics + chemistry) / 3.0;
+
+        System.out.println("Sams average mark in PCM is " + average);
+    }
+
+}
